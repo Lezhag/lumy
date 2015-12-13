ebe5ce01512cf2bf91c31a8ffca2b241920f6221
Oleg Glyb <oleg.glyb@gmail.com>
2015-12-14 01:29:24 +0200

    major clean-up new pics

--- Lumy.Website/scripts/image_gallery.js
+++ Lumy.Website/scripts/image_gallery.js
@@ -2,23 +2,6 @@
  * Created by Oleg on 31/10/2015.
  */
 $(document).ready(function() {
-    $('.popup-gallery').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-            titleSrc: function(item) {
-                return item.el.attr('title');
-            }
-        }
-    });
     $('.head-wreath-gallery').magnificPopup({
         delegate: 'a',
         type: 'image',
@@ -30,7 +13,7 @@ $(document).ready(function() {
             preload: [0,1] // Will preload 0 - before current, and 1 after the current image
         },
         image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded',
             titleSrc: function(item) {
                 return item.el.attr('title');
             }
@@ -47,7 +30,7 @@ $(document).ready(function() {
             preload: [0,1] // Will preload 0 - before current, and 1 after the current image
         },
         image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded',
             titleSrc: function(item) {
                 return item.el.attr('title');
             }
@@ -64,7 +47,7 @@ $(document).ready(function() {
             preload: [0,1] // Will preload 0 - before current, and 1 after the current image
         },
         image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded',
             titleSrc: function(item) {
                 return item.el.attr('title');
             }
@@ -81,10 +64,45 @@ $(document).ready(function() {
             preload: [0,1] // Will preload 0 - before current, and 1 after the current image
         },
         image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded',
+            titleSrc: function(item) {
+                return item.el.attr('title');
+            }
+        }
+    });
+    $('.about-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded',
+            titleSrc: function(item) {
+                return item.el.attr('title');
+            }
+        }
+    });
+    $('.corsage-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded',
             titleSrc: function(item) {
                 return item.el.attr('title');
             }
         }
     });
 });
+
